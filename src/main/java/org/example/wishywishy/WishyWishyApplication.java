954d308267e93f85293d4abeff04b4e30597348b
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-08 13:15:26 +0200

    Initial commit, added dependencies: Spring Web, Thymeleaf, H2 database, MySQL

---
+++ src/main/java/org/example/wishywishy/WishyWishyApplication.java
@@ -0,0 +1,13 @@
+package org.example.wishywishy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishyWishyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishyWishyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wishywishy/WishyWishyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wishywishy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishyWishyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
