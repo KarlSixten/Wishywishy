aaea57f602f39a5becb2119f6ed45a8b549401a5
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-08 15:22:12 +0200

    removed unused imports

--- src/main/java/org/example/wishywishy/controller/WishyController.java
+++ src/main/java/org/example/wishywishy/controller/WishyController.java
@@ -1,8 +1,6 @@
 package org.example.wishywishy.controller;
 
-import org.example.wishywishy.WishyWishyApplication;
 import org.example.wishywishy.service.WishyService;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
