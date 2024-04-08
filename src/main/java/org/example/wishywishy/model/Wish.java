2b6b4a3b3296c4f3505d895aa8ada2e6c37e9f77
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-08 15:05:58 +0200

    added Wish.java

---
+++ src/main/java/org/example/wishywishy/model/Wish.java
@@ -0,0 +1,10 @@
+package org.example.wishywishy.model;
+
+import java.net.URL;
+
+public class Wish {
+    private String name;
+    private String description;
+    private double price;
+    private URL url;
+}
