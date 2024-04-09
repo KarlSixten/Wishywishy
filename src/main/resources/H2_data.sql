2d8c028ace2ec3a2f7921637540bde78f3f7a682
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2024-04-09 09:49:28 +0200

    Changes by Josse

---
+++ src/main/resources/H2_data.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS wishywishy;
+CREATE DATABASE wishywishy;
+USE wishywishy;
+
+CREATE TABLE users
+(
+    username VARCHAR(45) NOT NULL PRIMARY KEY,
+    passcode VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE wishlist
+(
+    wishlistId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(45),
+    wishlistName VARCHAR(45),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE wish
+(
+    wishId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    wishName VARCHAR(45),
+    URL VARCHAR(45),
+    wishPrice DOUBLE,
+    wishlistId INT,
+    FOREIGN KEY (wishlistId) REFERENCES wishlist(wishlistId)
+);
