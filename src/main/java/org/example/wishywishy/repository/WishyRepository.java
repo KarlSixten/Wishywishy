e147530974116afff6e3ed4d344645bb2c73048d
Aleksandergreg <alek3691@stud.kea.dk>
2024-04-09 10:20:32 +0200

    Added delete method to repository and service class.

--- src/main/java/org/example/wishywishy/repository/WishyRepository.java
+++ src/main/java/org/example/wishywishy/repository/WishyRepository.java
@@ -1,7 +1,12 @@
 package org.example.wishywishy.repository;
 
+import org.example.wishywishy.repository.util.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Repository
 public class WishyRepository {
@@ -12,7 +17,16 @@ public class WishyRepository {
     @Value("${spring.datasource.password}")
     private String password;
 
+public void deleteWish (int wishId) throws SQLException {
+String sqlDeleteWish = "DELETE FROM wish WHERE wishID = ?;";
 
+    Connection connection = ConnectionManager.getConnection(url, user, password);
+
+try (PreparedStatement pstmtWish = connection.prepareStatement(sqlDeleteWish)){
+pstmtWish.setInt(1,wishId);
+pstmtWish.executeUpdate();
+}
+}
 
 
 }
--- src/main/java/org/example/wishywishy/service/WishyService.java
+++ src/main/java/org/example/wishywishy/service/WishyService.java
@@ -3,6 +3,8 @@ package org.example.wishywishy.service;
 import org.example.wishywishy.repository.WishyRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Service
 public class WishyService {
     private final WishyRepository wishyRepository;
@@ -10,4 +12,8 @@ public class WishyService {
     public WishyService(WishyRepository wishyRepository) {
         this.wishyRepository = wishyRepository;
     }
+
+public void deleteWish(int wishId) throws SQLException {
+    wishyRepository.deleteWish(wishId);
+}
 }
